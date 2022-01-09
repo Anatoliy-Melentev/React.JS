9d3fb9b55d39910bfc0977ae3877489cf42999a1
Anatoliy <muzhik.lapotnik@gmail.com>
2022-01-09 19:39:17 +0500

    2.3 Конфигурация HMR (Обновление кода без перезагузки страницы)

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
@@ -10,26 +12,36 @@ function setupDevtool() {
 }
 
 module.exports = {
+  watchOptions: {
+    ignored: /dist/
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: { 'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom' },
   },
   mode: NODE_ENV ? NODE_ENV : 'development',
-  entry: path.resolve(__dirname, '../src/client/index.jsx'),
+  entry: [
+    path.resolve(__dirname, '../src/client/index.jsx'),
+    'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+  ],
   output: {
     path: path.resolve(__dirname, '../dist/client'),
     filename: 'client.js',
+    publicPath: '/static/',
   },
   module: {
     rules: [{
       test: /\.[tj]sx?$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread']
-        }
-      },
+      //use: ['ts-loader']
+       use: {
+         loader: 'babel-loader',
+         options: {
+           presets: ['@babel/preset-env', '@babel/preset-react'],
+           plugins: ['@babel/plugin-proposal-object-rest-spread']
+         }
+       },
     }]
   },
   devtool: setupDevtool(),
+  plugins: IS_DEV ? [new CleanWebpackPlugin(), new HotModuleReplacementPlugin()] : [],
 };
